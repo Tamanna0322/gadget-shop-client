2afd9a2190f02e7b4038459c55d80adf391f3ecf
Tamanna0322 <tamanna0322@gmail.com>
2024-11-16 20:22:18 +0600

    added product about and contact pages

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home";
+import Products from "../pages/Products";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
 
 export const router = createBrowserRouter([
     {
@@ -10,7 +13,19 @@ export const router = createBrowserRouter([
         {
           path: "/",
           element:<Home></Home>
-        }
+        },
+        {
+          path: "/products",
+          element:<Products></Products>
+        },
+        {
+          path: "/about",
+          element:<About></About>
+        },
+        {
+          path: "/contact",
+          element:<Contact></Contact>
+        },
       ]
     },
   ]);
